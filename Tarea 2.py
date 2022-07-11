a3e09cae0b088a915477f82c66df407612a5b757
Inkev <inkev.ap@gmail.com>
2022-07-10 21:38:59 -0600

    Uso de For (imprime numero impares) solicita al usuario el numero

--- Tarea 2.py
+++ Tarea 2.py
@@ -1,6 +1,13 @@
-numero_inicial = 2
-numero_final = 8
+numero_inicial = int(input("Ingresa un numero: "))
+numero_final = int(input("Ingresa un numero mayor al anterior: "))
+numeros_impares = []
+while numero_final <= numero_inicial:
+    print("El numero que has ingresado es menor al primer numero")
+    numero_final = int(input("Ingresa un numero mayor al anterior: "))
 
 for numero_actual in range(numero_inicial,numero_final):
     if numero_actual % 2 == 1:
-        print(numero_actual)
+        numeros_impares.append(numero_actual)
+
+print("Numeros impares entre %s " % numero_inicial + "y %s" % numero_final)
+print(numeros_impares)
