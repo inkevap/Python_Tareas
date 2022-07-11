113c5ac23a7649c8cd83c4a6c1cba8d050f89cad
Inkev <inkev.ap@gmail.com>
2022-07-10 21:16:40 -0600

    Uso de For (imprime numero impares)

--- Tarea 2.py
+++ Tarea 2.py
@@ -1,6 +1,6 @@
-numero_inicial = 9
-numero_final = 22
+numero_inicial = 2
+numero_final = 8
 
 for numero_actual in range(numero_inicial,numero_final):
-    if numero_actual % 2 == 0:
+    if numero_actual % 2 == 1:
         print(numero_actual)
