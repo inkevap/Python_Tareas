ef54da90a50dc30b4f65439c81e4ecf5a48ed4e1
Inkev <inkev.ap@gmail.com>
2022-07-10 22:00:02 -0600

    Uso de for e inverse

---
+++ Tarea 3.py
@@ -0,0 +1,5 @@
+numero_inicial = int(input("Ingrese un numero inicial: "))
+numero_final = int(input("Ingrese un numero final: "))
+
+for x in range (numero_final,numero_inicial-1,-1):
+    print(x)
